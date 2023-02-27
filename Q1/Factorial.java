a1633e1ffed330d15e22e19e176a8eadb779d872
Satyam Jadhav <dishamurarka1117@gmail.com>
2023-02-27 18:07:11 +0530

    commit commit commit

---
+++ Q1/Factorial.java
@@ -0,0 +1,57 @@
+<<<<<<< HEAD
+
+//Java Program to find the Factorial of a Number
+
+import java.util.*;
+
+public class Factorial
+{
+     public static void main(String []args)
+     {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("\n Enter the number: ");
+
+        int num = scanner.nextInt();
+
+        int i=1, fact=1;
+
+        while(i<=num)
+        {
+            fact=fact*i;
+            i++;
+        } 
+
+        System.out.println("\n Factorial of the number: " + fact); 
+ 	System.out.println("Changes in this file are done"); 
+     }   
+=======
+
+//Java Program to find the Factorial of a Number
+
+import java.util.*;
+
+public class Factorial
+{
+     public static void main(String []args)
+     {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("\n Enter the number: ");
+
+        int num = scanner.nextInt();
+
+        int i=1, fact=1;
+
+        while(i<=num)
+        {
+            fact=fact*i;
+            i++;
+        } 
+
+        System.out.println("\n Factorial of the number: " + fact);  
+     }   
+>>>>>>> 3bc91eff8ae9b19b3350140c87e94ac17f33d682
+}
